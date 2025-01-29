cf2f1ecbf56c9441448c8101fc2c7a6c074a2d02
roleengineer <roleengineer@gmail.com>
2025-01-29 13:30:55 +0100

    (cm-group): refactored storage into EIP1967

---
+++ src/CoreMembersGroup/CoreMembersGroupStorage.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.28;
+
+contract CoreMembersGroupStorage {
+    // Constants
+
+    // keccak256(abi.encode(uint256(keccak256("circles.storage.CoreMembersGroup")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 internal constant STATE_STORAGE_SLOT = 0x91697a2af80a11a747c8f7c8c9fdbc08adbc198c96a843765cad94d605b7e400;
+
+    // State
+
+    /// @custom:storage-location erc7201:circles.storage.CoreMembersGroup
+    struct State {
+        address owner;
+        address ancillary;
+        address service;
+    }
+
+    function _state() internal pure returns (State storage state) {
+        bytes32 stateSlot = STATE_STORAGE_SLOT;
+        assembly {
+            state.slot := stateSlot
+        }
+    }
+}
