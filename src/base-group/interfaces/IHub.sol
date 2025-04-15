430f8158537e01fd02169210617fb1cb606c740f
roleengineer <roleengineer@gmail.com>
2025-04-15 16:15:14 +0200

    (interfaces): final refactoring

--- src/base-group/interfaces/IHub.sol
+++ src/base-group/interfaces/IHub.sol
@@ -1,32 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.28;
 
-import "circles-contracts-v2/hub/IHub.sol";
-import "circles-contracts-v2/hub/TypeDefinitions.sol";
-
-interface IHub is IHubV2 {
-    /// @notice registers group with Circles hub
-    function registerGroup(address policy, string calldata name, string calldata symbol, bytes32 metadataDigest)
-        external;
+interface IHub {
+    /// @notice returns the value of tokens of token type `id` owned by `account` at current day.
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+    /// @notice burns ERC1155 amount of id owned by caller.
+    function burn(uint256 id, uint256 amount, bytes calldata data) external;
+    /// @notice transfers a `value` amount of tokens of type `id` from `from` to `to`.
+    function safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data) external;
+    /// @notice register group with Circles hub
     function registerCustomGroup(
-        address _mint,
-        address _treasury,
-        string calldata _name,
-        string calldata _symbol,
-        bytes32 _metadataDigest
+        address mint,
+        address treasury,
+        string calldata name,
+        string calldata symbol,
+        bytes32 metadataDigest
     ) external;
     /// @notice register organization with Circles hub
     function registerOrganization(string calldata name, bytes32 metadataDigest) external;
     /// @notice trust sets the trust of the caller for the receiver with an expiry time.
-    function trust(address _trustReceiver, uint96 _expiry) external;
+    function trust(address trustReceiver, uint96 expiry) external;
     /// @notice isTrusted returns true if the expiry time of the trust relation is in the future
     function isTrusted(address truster, address trustee) external returns (bool);
-    /// @notice Trustmarkers returns (iterator, expiry uint96)
-    function trustMarkers(address, address) external returns (TypeDefinitions.TrustMarker memory);
-    /// @notice treasuries returns the collateral treasury of the group
-    function treasuries(address) external returns (address);
-    /// @notice sets advanced usage flags
-    function setAdvancedUsageFlag(bytes32 flag) external;
     /// @notice groupMint allows the holder of collateral to directly group mint
     function groupMint(
         address group,
@@ -34,5 +29,6 @@ interface IHub is IHubV2 {
         uint256[] calldata amounts,
         bytes calldata data
     ) external;
-    function wrap(address _avatar, uint256 _amount, uint8 _type) external returns (address);
+    /// @notice wraps ERC1155 token into ERC20 token
+    function wrap(address avatar, uint256 amount, uint8 circlesType) external returns (address);
 }
--- src/base-group/interfaces/INameRegistry.sol
+++ src/base-group/interfaces/INameRegistry.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.28;
 
-import "circles-contracts-v2/names/INameRegistry.sol";
-
-interface INameRegistryExtended is INameRegistry {
+interface INameRegistry {
     /// @notice updates metadata digest
     function updateMetadataDigest(bytes32 metadataDigest) external;
     /// @notice registers short name
